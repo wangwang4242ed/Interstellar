97c8985955621fc59021b2865c2605dde78787ee
wangwang4242ed <151289641+wangwang4242ed@users.noreply.github.com>
2024-02-15 06:06:56 +0000

    modified:   config.js 	new file:   main launch

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const config = {
   routes: true, // Change this to false if you just want to host a bare server.
   local: true, // Change this to false to disable local assets.
-  challenge: false,
+  challenge: true,
   users: {
     // username: 'password', you can add multiple users.
-    interstellar: 'password',
+    main: 'main',
   },
 }
 export default config
